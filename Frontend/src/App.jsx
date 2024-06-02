6d31dca66f78bc7452db294a71bea673fdec794f
Ricardo <descoubet0107@gmail.com>
2024-06-02 01:47:20 -0400

    scroll bar off

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { AppRouter } from './routes/AppRouter.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'animate.css/animate.min.css';
+import './styles.css';
 
 function App() {
 	return (
