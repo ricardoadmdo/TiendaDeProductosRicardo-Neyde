05c25ba009ba66123bdb03aee0c1cb20adf0a155
Ricardo <descoubet0107@gmail.com>
2024-06-03 23:54:56 -0400

    icono footer de whataspp a color verde chillon

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -3,13 +3,19 @@ import { FaWhatsapp, FaFacebook, FaInstagram } from 'react-icons/fa';
 const Footer = () => {
 	const year = new Date().getFullYear();
 	return (
-		<footer className=' footer-margin bg-dark text-center text-white py-4'>
+		<footer className='footer-margin bg-dark text-center text-white py-4'>
 			<div className='container'>
 				<h2>Contacto</h2>
 				<p>Conéctate con nosotros:</p>
 				<div className='d-flex justify-content-center'>
-					<a className='text-decoration-none text-success mx-3' href='https://wa.me/+5358175289' target='_blank' rel='noopener noreferrer'>
-						<FaWhatsapp size={30} /> Whatsapp
+					<a
+						style={{ color: '#00FF00' }}
+						className='text-decoration-none mx-3'
+						href='https://wa.me/+5358175289'
+						target='_blank'
+						rel='noopener noreferrer'
+					>
+						<FaWhatsapp size={30} style={{ color: '#00FF00' }} /> Whatsapp
 					</a>
 					<a
 						className='text-decoration-none text-primary mx-3'
