622179d92daede81c7f21992ec24a5f9a1d5d955
Ricardo <descoubet0107@gmail.com>
2024-06-04 00:15:50 -0400

    cambios de rendimiento contenfull painting

--- Frontend/src/components/ui/Bienvenida.jsx
+++ Frontend/src/components/ui/Bienvenida.jsx
@@ -14,17 +14,24 @@ export const Bienvenida = () => {
 					<h2>Explora Nuestros Productos</h2>
 					<div className='card mb-4'>
 						<LazyLoadImage
-							threshold={10}
-							effect='blur'
+							effect='opacity'
 							placeholderSrc={placeholder}
 							src='https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/dtybbo5xcuw8m4ur1tib.jpg'
 							className='card-img-top img-fluid'
 							alt='Imagen del producto'
 							style={{ objectFit: 'cover' }}
+							width='600'
+							height='400'
+							loading='eager'
+							srcSet='
+								https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/dtybbo5xcuw8m4ur1tib.jpg 600w,
+								https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/dtybbo5xcuw8m4ur1tib_small.jpg 300w
+							'
+							sizes='(max-width: 600px) 300px, 600px'
 						/>
 						<div className='card-body'>
 							<p className='card-text'>Descubre nuestra amplia gama de productos seleccionados con la mayor calidad.</p>
-							<Link to='/productos' className='btn btn-primary'>
+							<Link to='/productos' className='btn btn-dark'>
 								Ver Productos
 							</Link>
 						</div>
@@ -35,17 +42,24 @@ export const Bienvenida = () => {
 					<h2>Combos Especiales</h2>
 					<div className='card mb-3'>
 						<LazyLoadImage
-							threshold={10}
-							effect='blur'
+							effect='opacity'
 							placeholderSrc={placeholder}
 							src='https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/kkplmrpcfpfftbg1lbby.jpg'
 							className='card-img-top img-fluid'
 							alt='Imagen del producto'
 							style={{ objectFit: 'cover' }}
+							width='600'
+							height='400'
+							loading='eager'
+							srcSet='
+								https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/kkplmrpcfpfftbg1lbby.jpg 600w,
+								https://res.cloudinary.com/dber1pxea/image/upload/v1716711600/kkplmrpcfpfftbg1lbby_small.jpg 300w
+							'
+							sizes='(max-width: 600px) 300px, 600px'
 						/>
 						<div className='card-body'>
 							<p className='card-text'>Prueba nuestros irresistibles combos, perfectos para cualquier ocasión.</p>
-							<Link to='/combos' className='btn btn-primary'>
+							<Link to='/combos' className='btn btn-dark'>
 								Ver Combos
 							</Link>
 						</div>
@@ -63,13 +77,20 @@ export const Bienvenida = () => {
 				<hr />
 				<div className='card mb-4'>
 					<LazyLoadImage
-						threshold={10}
-						effect='blur'
+						effect='opacity'
 						placeholderSrc={placeholder}
 						src='https://res.cloudinary.com/dber1pxea/image/upload/v1716711597/kjgjpyl3rf4drlm6uthd.jpg'
 						className='card-img-top img-fluid'
 						alt='Imagen del producto'
 						style={{ height: '200px', objectFit: 'cover' }}
+						width='600'
+						height='200'
+						loading='eager'
+						srcSet='
+							https://res.cloudinary.com/dber1pxea/image/upload/v1716711597/kjgjpyl3rf4drlm6uthd.jpg 600w,
+							https://res.cloudinary.com/dber1pxea/image/upload/v1716711597/kjgjpyl3rf4drlm6uthd_small.jpg 300w
+						'
+						sizes='(max-width: 600px) 300px, 600px'
 					/>
 					<div className='card-body text-center'>
 						<p className='card-text'>Nuestra tienda en San Miguel, calle Delicia, Las Piedras, La Habana, Cuba.</p>
@@ -82,12 +103,13 @@ export const Bienvenida = () => {
 				<h2>Encuéntranos en La Habana</h2>
 				<p>Visita nuestra tienda en San Miguel, calle Delicia, Las Piedras, La Habana, Cuba.</p>
 				<div
+					className='my-4'
 					dangerouslySetInnerHTML={{
 						__html: `
         <iframe
 		  title='ubicación google maps de la tienda'
-          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3670.7048252406435!2d-82.29946848126039!3d23.071280811154583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDA0JzE2LjciTiA4MsKwMTcnNTguMyJX!5e0!3m2!1ses-419!2scu!4v1716698429508!5m2!1ses-419!2scu"
-          width="600"
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3670.7048252406435!2d-82.29946848126039!3d23.071280811154583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjPCsDA0JzE2LjciTiA4MsKwMTcnNTguMyJX!5e0!3m2!1ses-419!2scu!4v1716698429508!5m2!1ses-419!2scu&maptype=terrain"
+          width="100%"
           height="450"
           style="border:0"
           allowFullScreen=""
