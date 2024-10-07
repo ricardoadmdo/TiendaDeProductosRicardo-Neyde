da3578e9247554e8e93deb6e039c9bd45652dd01
Ricardo <descoubet0107@gmail.com>
2024-10-07 13:12:54 -0400

    a

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import compression from 'vite-plugin-compression';
 
 // export default defineConfig({
 // 	plugins: [
